d4298df0cc35da74b8eccd608a858b71afde3637
Garrett Lander <garrett.jlander@gmail.com>
2024-08-28 10:13:02 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Card from './components/Card';
+
+const App = () => {
+    return (
+        <div>
+            <h1 className='text-center text-7xl m-6'>Dark Mode!</h1>
+            <Card />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,29 @@
+const Card = () => {
+    return (
+        <div className='shadow-xl hover:shadow-2xl hover:cursor-pointer w-1/2 max-w-[500px] rounded-md m-auto p-10 bg-slate-200 dark:bg-slate-900'>
+            <figure className='rounded-t-md overflow-hidden'>
+                <img
+                    className='w-full'
+                    src='https://cdn.openart.ai/published/0F291PE3J060JXZAjHX1/4qP6A5RM_bjDX_1024.webp'
+                    alt='Red Dragonborn Paladin'
+                />
+            </figure>
+            <div className='flex flex-col px-4 py-2 rounded-b-md bg-slate-100 dark:bg-slate-800'>
+                <h2 className='text-4xl border-b-2 mb-4 border-b-gray-400'>
+                    Kaladan the Paladin
+                </h2>
+                <ul>
+                    <li className='my-1 text-xl'>Level: 12</li>
+                    <li className='my-1 text-xl'>Race: Dragonborn</li>
+                    <li className='my-1 text-xl'>Class: Paladin</li>
+                    <li className='my-1 text-xl'>Background: Noble</li>
+                </ul>
+                <button className='self-end py-2 px-4 rounded-md text-white bg-violet-500 dark:bg-red-800'>
+                    See more
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
